561a7d64914f37ad04cfd0f2d56ab289b9e1b21a
Dmitry Karasik <dmitry@karasik.eu.org>
2022-07-01 07:44:45 +0200

    fixes for win32

--- lib/Net/MitDK.pm
+++ lib/Net/MitDK.pm
@@ -496,9 +496,11 @@ sub save
 	unless ( -d $home ) {
 		mkdir $home or return (undef, "Cannot create $home: $!");
 		return (undef, "cannot chmod 0750 $home:$!") unless chmod 0750, $home;
-		my ($name,$passwd,$uid,$gid) = getpwnam('nobody');
-		return (undef, "no user `nobody`") unless defined $name;
-		return (undef, "cannot chown user:$name $home:$!") unless chown $>, $gid, $home;
+		if ( $^O !~ /win32/i) {
+			my ($name,$passwd,$uid,$gid) = getpwnam('nobody');
+			return (undef, "no user `nobody`") unless defined $name;
+			return (undef, "cannot chown user:$name $home:$!") unless chown $>, $gid, $home;
+		}
 	}
 
 	my $json;
